eeb7ecc5a1ac86ea550d53ea1e84065ef388ddd6
Gregor Aisch <gka@users.noreply.github.com>
2020-06-09 11:37:43 +0200

    change author to Datawrapper GmbH

--- datawrapper-oembed/datawrapper-oembed.php
+++ datawrapper-oembed/datawrapper-oembed.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * @package Datawrapper
- * @version 1.0
+ * @version 1.1
  */
 /*
 Plugin Name: Datawrapper OEmbed
 Plugin URI: http://wordpress.org/plugins/datawrapper-oembed/
 Description: Registers Datawrapper as oembed provider so charts, maps, and tables created with Datawrapper can be embedded in WordPress.
-Author: Gregor Aisch
+Author: Datawrapper GmbH
 Version: 1.0
 Author URI: https://www.datawrapper.de/
 License: GPLv2 or later
